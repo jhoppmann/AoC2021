bca68c0e7abc0a377d222fc71742d35609cd5018
Jan Hoppmann <jhoppmann@impossiblynamed.de>
2021-12-01 07:52:53 +0100

    Day 1

---
+++ day01/day01.py
@@ -0,0 +1,17 @@
+with open('input.txt') as file:
+    depths = file.read().splitlines()
+
+depths = [int(x) for x in depths]
+
+
+
+def main():
+    increases = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i-1]:
+            increases += 1
+    print(increases)
+
+
+if __name__ == "__main__":
+    main()
