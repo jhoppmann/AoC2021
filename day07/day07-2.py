4f94d394e2b2aee3665b060692a6c7df76b45df6
Jan Hoppmann <jhoppmann@impossiblynamed.de>
2021-12-07 06:30:47 +0100

    solutions for day 7

---
+++ day07/day07-2.py
@@ -0,0 +1,23 @@
+def main() -> None:
+    with open('input.txt') as file:
+        crabs = [int(x) for x in file.read().split(',')]
+
+    min_val = min(crabs)
+    max_val = max(crabs)
+    fuel_usage = len(crabs) * gauss_formula(max_val)
+    for i in range(min_val, max_val + 1):
+        fuel = 0
+        for crab in crabs:
+            fuel += gauss_formula(abs(crab - i))
+
+        fuel_usage = min(fuel, fuel_usage)
+
+    print(fuel_usage)
+
+
+def gauss_formula(n: int) -> float:
+    return int((n * (n + 1)) / 2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day07/day07.py
@@ -0,0 +1,19 @@
+def main() -> None:
+    with open('input.txt') as file:
+        crabs = [int(x) for x in file.read().split(',')]
+
+    min_val = min(crabs)
+    max_val = max(crabs)
+    fuel_usage = len(crabs) * max_val
+    for i in range(min_val, max_val + 1):
+        fuel = 0
+        for crab in crabs:
+            fuel += abs(crab - i)
+
+        fuel_usage = min(fuel, fuel_usage)
+
+    print(fuel_usage)
+
+
+if __name__ == '__main__':
+    main()
